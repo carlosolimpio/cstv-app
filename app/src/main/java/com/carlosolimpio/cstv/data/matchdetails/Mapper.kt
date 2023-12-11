a57559e1415d50fe22e517acb1152cb61f421aff
Carlos Olimpio <carlossolimpio@gmail.com>
2023-12-11 14:59:28 -0300

    Quick fix the mapper for empty player info
    - add "no info" text to fields
    - adjust player layout to fit the text without breaking

--- app/src/main/java/com/carlosolimpio/cstv/data/matchdetails/Mapper.kt
+++ app/src/main/java/com/carlosolimpio/cstv/data/matchdetails/Mapper.kt
@@ -10,7 +10,25 @@ fun TeamPlayersDto.toTeamPlayers() = TeamPlayers(
     players = players.toPlayerList()
 )
 
-fun List<PlayerDto>.toPlayerList() = this.map { it.toPlayer() }
+fun List<PlayerDto>.toPlayerList() = this.fillLineUp().map { it.toPlayer() }
+
+fun List<PlayerDto>.fillLineUp(): List<PlayerDto> {
+    val FULL_LINEUP = 5
+    if (this.size < FULL_LINEUP) {
+        val playersCount = FULL_LINEUP - this.size
+        val additionalPlayers = fillWithDummyPlayers(playersCount)
+        return this + additionalPlayers
+    }
+    return this
+}
+
+private fun fillWithDummyPlayers(count: Int): List<PlayerDto> {
+    val additionalPlayers = mutableListOf<PlayerDto>()
+    for (i in 1..count) {
+        additionalPlayers.add(PlayerDto("No Info", "No Info", "", ""))
+    }
+    return additionalPlayers
+}
 
 fun PlayerDto.toPlayer() = Player(
     nickName = nickname ?: "",
